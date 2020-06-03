1e7bc029258294f41aeee34d8f879727bf64b392
leoalmeida95 <leo.almeida.silva@hotmail.com>
2020-06-03 15:59:48 -0300

    obtendo o json da codenation

--- app.py
+++ app.py
@@ -1,27 +1,30 @@
-from flask import Flask
-from flask_restful import Api, Resource
+import json
 import requests
 import os
 from os.path import dirname, isfile, join
+from flask import Flask
+from flask_restful import Api, Resource
 from dotenv import load_dotenv
-
-app = Flask(__name__)
-app.config["APPLICATION_ROOT"] = "/cripto"
 
 # a partir do arquivo atual adicione ao path o arquivo .env
 _ENV_FILE = join(dirname(__file__), '.env')
-
 # existindo o arquivo faça a leitura do arquivo através da função load_dotenv
 if isfile(_ENV_FILE):
     load_dotenv(dotenv_path=_ENV_FILE)
 
+app = Flask(__name__)
 api = Api(app)
     
 @app.route('/', methods=['GET'])
 def get():
     url = 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token={}'.format(os.getenv('TOKEN'))
     response = requests.get(url)
-    print(response)
+
+    if(response.status_code == 200):
+        json = response.json()
+
+    else:
+        print("Erro ao obter o json")
     #data=''
     #url = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=' + getenv('TOKEN')
     #headers = {'Content-type': 'multipart/form-data; charset=UTF-8'}
