b71dc3893b35217f8bf5d9b8ac2bfd7657cc4340
leoalmeida95 <leo.almeida.silva@hotmail.com>
2020-06-03 15:26:13 -0300

    montando a url de requisição

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
 from flask import Flask
 from flask_restful import Api, Resource
 import requests
-from os import getenv
+import os
+from os.path import dirname, isfile, join
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 app.config["APPLICATION_ROOT"] = "/cripto"
 
+# a partir do arquivo atual adicione ao path o arquivo .env
+_ENV_FILE = join(dirname(__file__), '.env')
+
+# existindo o arquivo faça a leitura do arquivo através da função load_dotenv
+if isfile(_ENV_FILE):
+    load_dotenv(dotenv_path=_ENV_FILE)
+
 api = Api(app)
-
+    
 @app.route('/', methods=['GET'])
-def get(self):
-    data=''
-    url = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=' + getenv('TOKEN')
-    headers = {'Content-type': 'multipart/form-data; charset=UTF-8'}
-    response = requests.post(url, data=data, headers=headers)
-    return {'site': 'APP'}
-
-api.add_resource('/codenation', 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=' + getenv('TOKEN'))
+def get():
+    url = 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token={}'.format(os.getenv('TOKEN'))
+    response = requests.get(url)
+    print(response)
+    #data=''
+    #url = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=' + getenv('TOKEN')
+    #headers = {'Content-type': 'multipart/form-data; charset=UTF-8'}
+    #response = requests.post(url, data=data, headers=headers)
+    return "OBTENDO JSON"
 
 if __name__ == '__main__':
-    app.run(debug=getenv('DEBUG'))
+    app.run(debug=os.getenv('DEBUG'))
